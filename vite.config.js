9eebe74184dbeff6bdd442502ead2ae76fd6b0ac
Joseph <josephgreat2003@gmail.com>
2024-05-02 22:00:09 +0100

    secured api keys

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    'process.env.REACT_APP_API_KEY': JSON.stringify(process.env.REACT_APP_API_KEY),
+    'process.env.REACT_APP_SEARCH_ENGINE_ID': JSON.stringify(process.env.REACT_APP_SEARCH_ENGINE_ID)
+  }
+});
